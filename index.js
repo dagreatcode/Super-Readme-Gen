39962be3aa5ac9e610bb30c8a73d496aaed37b74
dagreatcode <dagreatcode@gmail.com>
2020-09-24 18:50:54 -0400

    updating the questions

--- index.js
+++ index.js
@@ -5,13 +5,32 @@ const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
 
+// const writeFileAsync = util.promisify(fs.writeFile);
+
 // array of questions for user
 const questions = [
-
+    inquirer.prompt([
+        {
+        name: "userName",
+        type: "input",
+        message: "What is your name?",
+        },
+        {
+        name: "hobby",
+        type: "input",
+        message: "What is your favorite hobby?",
+        },
+        {
+        name: "linkedIn",
+        type: "input",
+        message: "What is your LinkedIn URL?",
+        },
+    ])
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return ``;
 }
 
 // function to initialize program
