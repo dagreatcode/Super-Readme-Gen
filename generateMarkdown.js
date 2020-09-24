2ec1744efc3bb20d5d18c337d513eda3d2405f63
dagreatcode <dagreatcode@gmail.com>
2020-09-24 17:20:17 -0400

    sudo and testing

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,7 +1,19 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let licenseText = "";
+  if(data.license === "MIT"){
+    licenseText = "This application is licensed under the MIT License . . .";
+    // [![made-with-Markdown](https://img.shields.io/badge/Made%20with-Markdown-1f425f.svg)](http://commonmark.org)
+    // [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  }
 
+  return `# ${data.title}
+# 
+##
+###
+####
+${data.license}
+MIT
 `;
 }
 
